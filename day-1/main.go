dbb0dd69e9d505ac75141b00f05b736d62ce014b
Émile Nestor <eav.nestor@gmail.com>
2022-12-01 08:33:35 +0100

    day-1

--- day-1/main.go
+++ day-1/main.go
@@ -1,5 +1,48 @@
 package main
 
-func main() {
+import (
+	"bufio"
+	"log"
+	"os"
+	"sort"
+	"strconv"
+)
 
+func main() {
+	file, err := os.Open("day-1/input.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+
+	elves := []int{}
+	sum := 0
+
+	for scanner.Scan() {
+		line := scanner.Text()
+
+		if line == "" {
+			elves = append(elves, sum)
+			sum = 0
+			continue
+		}
+
+		num, err := strconv.Atoi(line)
+		if err != nil {
+			log.Print(err)
+		}
+		sum += num
+	}
+
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+
+	sort.Slice(elves, func(i, j int) bool {
+		return elves[i] > elves[j]
+	})
+
+	log.Print(elves[0] + elves[1] + elves[2])
 }
