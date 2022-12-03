07f6f41a231e4b4172f28fe6c9455799d0637b6b
Émile Nestor <eav.nestor@gmail.com>
2022-12-03 16:22:59 +0100

    day-2 refactor

--- day-2/main.go
+++ day-2/main.go
@@ -11,22 +11,84 @@ const (
 	bPaper   = "B"
 	cScissor = "C"
 
+	xRock    = "X"
+	yPaper   = "Y"
+	zScissor = "Z"
+
 	lose = "X"
 	draw = "Y"
 	win  = "Z"
 )
 
 func main() {
-	file, err := os.Open("day-2/input.txt")
+	filePath := "day-2/input.txt"
+
+	log.Print("First Star: ", firstStar(filePath))
+	log.Print("Second Star: ", secondStar(filePath))
+}
+
+func firstStar(filePath string) int {
+	file, err := os.Open(filePath)
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer file.Close()
 
+	sum := 0
 	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line := scanner.Text()
+
+		switch string(line[2]) {
+		case xRock:
+			sum += 1
+			switch string(line[0]) {
+			case aRock:
+				sum += 3
+			case bPaper:
+				sum += 0
+			case cScissor:
+				sum += 6
+			}
+		case yPaper:
+			sum += 2
+			switch string(line[0]) {
+			case aRock:
+				sum += 6
+			case bPaper:
+				sum += 3
+			case cScissor:
+				sum += 0
+			}
+		case zScissor:
+			sum += 3
+			switch string(line[0]) {
+			case aRock:
+				sum += 0
+			case bPaper:
+				sum += 6
+			case cScissor:
+				sum += 3
+			}
+		}
+	}
+
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+
+	return sum
+}
+
+func secondStar(filePath string) int {
+	file, err := os.Open(filePath)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
 
 	sum := 0
-
+	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
 		line := scanner.Text()
 
@@ -65,5 +127,5 @@ func main() {
 		log.Fatal(err)
 	}
 
-	log.Print("Second Star: ", sum)
+	return sum
 }
