44a4b51845088ab46e905f3b9be87b220f07ce6b
Émile Nestor <eav.nestor@gmail.com>
2022-12-08 22:41:49 +0100

    day-7 and day-8

--- day-7/main.go
+++ day-7/main.go
@@ -5,13 +5,17 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"regexp"
+	"sort"
+	"strconv"
+	"strings"
 )
 
 func main() {
-	filePath := "day-0/input.txt"
+	filePath := "day-7/input.txt"
 
 	log.Print("First Star: ", firstStar(filePath))
-	log.Print("Second Star: ", secondStar(filePath))
+	// log.Print("Second Star: ", secondStar(filePath))
 }
 
 func firstStar(filePath string) int {
@@ -21,18 +25,108 @@ func firstStar(filePath string) int {
 	}
 	defer file.Close()
 
-	num := 0
+	fileMap := make(map[string]int)
+	dirMap := make(map[string]int)
+	dirMap["/"] = 0
+
+	path := "/"
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
 		line := scanner.Text()
 		fmt.Println(line)
+		re := regexp.MustCompile("[0-9]+")
+
+		cmdLine := strings.Split(line, " ")
+		if string(cmdLine[0]) == "$" {
+			switch string(cmdLine[1]) {
+			case "cd":
+				switch {
+				case string(cmdLine[2]) == "..":
+					split := strings.Split(path, "/")
+					i := len(split[len(split)-1])
+					if len(split) > 1 {
+						i++
+					}
+					fmt.Println(path, " to ", path[:len(path)-i])
+					path = path[:len(path)-i]
+					if !strings.HasPrefix(path, "/") {
+						path = fmt.Sprintf("%s%s", "/", path)
+					}
+				default:
+					if !strings.HasPrefix(path, "/") {
+						path = fmt.Sprintf("%s%s", "/", path)
+					}
+					if len(path) > 1 {
+						path += "/" + string(cmdLine[2])
+					} else if cmdLine[2] != "/" {
+						path += string(cmdLine[2])
+					}
+				}
+			}
+		}
+
+		if cmdLine[0] == "dir" {
+			if path == "/" {
+				dirMap[path+cmdLine[1]] = 0
+			} else {
+				dirMap[path+"/"+cmdLine[1]] = 0
+			}
+		}
+
+		isNum := re.Match([]byte(string(cmdLine[0])))
+		if isNum {
+			size, _ := strconv.Atoi(string(cmdLine[0]))
+			if path == "/" {
+				fileMap[path+cmdLine[1]] += size
+			} else {
+				fileMap[path+"/"+cmdLine[1]] += size
+			}
+		}
+
+		// IF file add to map[filepath]filesize
 	}
 
 	if err := scanner.Err(); err != nil {
 		log.Fatal(err)
 	}
 
-	return num
+	for dir := range dirMap {
+		for file, size := range fileMap {
+			if strings.HasPrefix(file, dir) {
+				dirMap[dir] += size
+			}
+		}
+	}
+
+	// Sum all values below 100000
+	sum := 0
+	for _, size := range dirMap {
+		if size < 100000 {
+			sum += size
+		}
+	}
+
+	keys := make([]string, 0, len(dirMap))
+
+	for key := range dirMap {
+		keys = append(keys, key)
+	}
+	sort.SliceStable(keys, func(i, j int) bool {
+		return dirMap[keys[i]] < dirMap[keys[j]]
+	})
+	required := (dirMap["/"] - (70000000 - 30000000))
+	allFiles := dirMap["/"]
+	fmt.Println("Space Required: ", required)
+	fmt.Println("Main: ", allFiles)
+	for _, k := range keys {
+		fmt.Println("Directory size: ", dirMap[k])
+		if required <= dirMap[k] {
+			sum = dirMap[k]
+			break
+		}
+	}
+
+	return sum
 }
 
 func secondStar(filePath string) int {
--- day-8/main.go
+++ day-8/main.go
@@ -5,53 +5,185 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strconv"
 )
 
+type TreeArray struct {
+	rows []string
+}
+
+var filePath string = "day-8/input.txt"
+
 func main() {
-	filePath := "day-0/input.txt"
+	treeStringArray := TreeArray{rows: []string{}}
 
-	log.Print("First Star: ", firstStar(filePath))
-	log.Print("Second Star: ", secondStar(filePath))
+	// Populate tree array
+	treeStringArray.getTreeStringArray()
+	// for _, row := range treeStringArray.rows {
+	// 	fmt.Println(row)
+	// }
+
+	firstStarResult := treeStringArray.visibleTreesCount()
+
+	log.Print("First Star: ", firstStarResult)
+	log.Print("Second Star: ", treeStringArray.bestTree())
 }
 
-func firstStar(filePath string) int {
+func (t *TreeArray) getTreeStringArray() {
 	file, err := os.Open(filePath)
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer file.Close()
 
-	num := 0
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
 		line := scanner.Text()
-		fmt.Println(line)
+		t.rows = append(t.rows, line)
 	}
 
 	if err := scanner.Err(); err != nil {
 		log.Fatal(err)
 	}
-
-	return num
 }
 
-func secondStar(filePath string) int {
-	file, err := os.Open(filePath)
-	if err != nil {
-		log.Fatal(err)
+func (t *TreeArray) getVerticalRow(index int) string {
+	row := ""
+	for i := 0; i < len(t.rows); i++ {
+		row += string(t.rows[i][index])
 	}
-	defer file.Close()
+	return row
+}
 
-	num := 0
-	scanner := bufio.NewScanner(file)
-	for scanner.Scan() {
-		line := scanner.Text()
-		fmt.Println(line)
+func (t *TreeArray) visibleTreesCount() int {
+	count := 0
+
+	// Add outer edge
+	count += (len(t.rows) * 2) + (len(t.rows[0]) * 2) - 4
+	fmt.Println("Trees visible on the outer edge: ", count)
+
+	for y := 1; y < len(t.rows)-1; y++ {
+		for i := 1; i < len(t.rows[y])-1; i++ {
+			// fmt.Println("=================")
+			row := t.rows[y]
+			// fmt.Println(row)
+
+			height, _ := strconv.Atoi(string(t.rows[y][i]))
+			visible1 := treeVisible(row, height, i)
+
+			// fmt.Printf("horizontal - %v\n", visible1)
+
+			row = t.getVerticalRow(i)
+			height, _ = strconv.Atoi(string(t.rows[y][i]))
+			// fmt.Println(row)
+			visible2 := treeVisible(row, height, y)
+
+			// fmt.Printf("vertical - %v\n", visible2)
+
+			visible := (visible1 || visible2)
+
+			// fmt.Printf("[%d] visibility: %v\n", height, visible)
+
+			if visible {
+				count++
+			}
+		}
 	}
 
-	if err := scanner.Err(); err != nil {
-		log.Fatal(err)
+	return count
+}
+
+func (t *TreeArray) bestTree() int {
+	count := 0
+
+	for y := 1; y < len(t.rows)-1; y++ {
+		for i := 1; i < len(t.rows[y])-1; i++ {
+			sum := 0
+			row := t.rows[y]
+
+			height, _ := strconv.Atoi(string(t.rows[y][i]))
+
+			//LOOK LEFT
+			left := 0
+			for j := i - 1; j >= 0; j-- {
+				tree, _ := strconv.Atoi(string(row[j]))
+				if tree >= height {
+					left++
+					break
+				}
+				left++
+			}
+
+			//LOOK RIGHT
+			right := 0
+			for j := i + 1; j <= len(row)-1; j++ {
+				tree, _ := strconv.Atoi(string(row[j]))
+				if tree >= height {
+					right++
+					break
+				}
+				right++
+			}
+
+			row = t.getVerticalRow(i)
+
+			//LOOK UP
+			up := 0
+			for j := y - 1; j >= 0; j-- {
+				tree, _ := strconv.Atoi(string(row[j]))
+				if tree >= height {
+					up++
+					break
+				}
+				up++
+			}
+
+			//LOOK DOWN
+			down := 0
+			for j := y + 1; j <= len(row)-1; j++ {
+				tree, _ := strconv.Atoi(string(row[j]))
+				if tree >= height {
+					down++
+					break
+				}
+				down++
+			}
+
+			sum = up * down * left * right
+			fmt.Printf("[%d]\n", height)
+			fmt.Printf("up %d, left %d, right %d, down %d\n", up, left, right, down)
+			fmt.Println(sum)
+
+			if count < sum {
+				count = sum
+			}
+		}
 	}
 
-	return num
+	return count
+}
+
+func treeVisible(row string, height int, index int) bool {
+	visible1 := true
+	visible2 := true
+	for i, tree := range row {
+		if i == index {
+			// fmt.Printf("[%d]", height)
+			continue
+		}
+		treeHeight, _ := strconv.Atoi(string(tree))
+		// fmt.Print(treeHeight)
+		if treeHeight >= height {
+			if i < index {
+				visible1 = false
+			}
+			if i > index {
+				visible2 = false
+			}
+		}
+	}
+	// fmt.Println()
+	// fmt.Printf("\nVISIBILITY")
+	// fmt.Printf("\n%v<-o->%v\n", visible1, visible2)
+	return visible1 || visible2
 }
